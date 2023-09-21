c34f8ade4f4a789fcdbd5c3a6351f8d2d9d24a20
hasindu karunathilaka <hasindukarunathilaka343@gmail.com>
2023-09-21 09:37:42 +0530

    Initial commit

---
+++ src/Actors/Actor.gd
@@ -0,0 +1,12 @@
+extends KinematicBody2D
+class_name Actor
+
+#set const value for jump
+const FLOOR_NORMAL: = Vector2.UP
+
+#pahalata yana speed eka
+export var gravity: = 4000.0
+#maximum speed eka
+export var speed: = Vector2(300.0, 1000.0)
+
+var _velocity: =	Vector2.ZERO
